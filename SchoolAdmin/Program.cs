2195fff1e5af8b2043b44fa3444d2fe92d6c130d
Thijs Jonas (s141821) <jonas.thijs@student.ap.be>
2023-03-05 20:48:08 +0100

    StudentKlasse

--- SchoolAdmin/Program.cs
+++ SchoolAdmin/Program.cs
@@ -4,25 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Student student1 = new Student();
-            student1.Naam = "Said Aziz";
-            student1.GeboorteDatum = new DateTime(2000, 6, 1);
-            student1.Studentennummer = Student.Studententeller;
-            Student.Studententeller++;
-            student1.RegistreerVoorCursus("Programmeren");
-            student1.RegistreerVoorCursus("Databanken");
-            
-            Student student2 = new Student();
-            student2.Naam = "Mieke Vermeulen";
-            student2.GeboorteDatum = new DateTime(1998, 1, 1);
-            student2.Studentennummer = Student.Studententeller;
-            Student.Studententeller++;
-            student2.RegistreerVoorCursus("Communicatie");
+            Console.WriteLine("Wat wil je doen?");
+            Console.WriteLine("1. DemonstreerStudenten uitvoeren");
+            byte choice = Convert.ToByte(Console.ReadLine());
 
-            Console.WriteLine(student1.GenereerNaamKaartje());
-            Console.WriteLine(student2.GenereerNaamKaartje());
-            Console.WriteLine(student1.BepaalWerkbelasting());
-            Console.WriteLine(student2.BepaalWerkbelasting());
+            switch (choice)
+            {
+                case 1:
+                    Student.DemonstreerStudenten();
+                    break;
+            }
         }
     }
 }
--- SchoolAdmin/Student.cs
+++ SchoolAdmin/Student.cs
@@ -8,20 +8,16 @@ namespace SchoolAdmin
 {
     internal class Student
     {
+        private string[] cursussen = new string[5];
+        public byte[] CursusResultaten = new byte[5];
         public string Naam;
         public DateTime GeboorteDatum;
         public uint Studentennummer;
-        private string[] cursussen = new string[5];
         public static uint Studententeller = 1;
 
-        public string GenereerNaamKaartje()
+        public byte BepaalWerkbelasting()
         {
-            return $"{this.Naam} (STUDENT)";
-        }
-
-        public double BepaalWerkbelasting()
-        {
-            double totaal = 0.0;
+            byte totaal = 0;
 
             for (int i = 0; i < this.cursussen.Length; i++)
             {
@@ -34,6 +30,41 @@ namespace SchoolAdmin
             return totaal;
         }
 
+        public double Gemiddelde(string[] cursussen)
+        {
+            double gemiddelde = 0.0;
+            byte aantalCursussen = 0;
+
+            for (int i = 0; i < cursussen.Length; i++)
+            {
+                if (cursussen[i] is not null)
+                {
+                    gemiddelde += Convert.ToDouble(CursusResultaten[i]);
+                    aantalCursussen++;
+                }
+            }
+
+            gemiddelde /= aantalCursussen;
+            return gemiddelde;
+        }
+
+        public string GenereerNaamKaartje()
+        {
+            return $"{this.Naam} (STUDENT)";
+        }
+
+        public void Kwoteer(byte cursusIndex, byte behaaldCijfer)
+        {
+            if (behaaldCijfer > 20)
+            {
+                Console.WriteLine("Ongeldig cijfer!");
+            }
+            else
+            {
+                this.CursusResultaten[cursusIndex] = behaaldCijfer;
+            }
+        }
+
         public void RegistreerVoorCursus(string cursus)
         {
             for (int i = 0; i < this.cursussen.Length; i++)
@@ -44,6 +75,49 @@ namespace SchoolAdmin
                     break;
                 }
             }
+        }
+
+        public void ToonOverzicht()
+        {
+            ushort age = Convert.ToUInt16(DateTime.Now.Year - this.GeboorteDatum.Year);
+            Console.WriteLine($"{this.Naam}, {age} jaar\n");
+            Console.WriteLine("Cijferrapport:\n**************");
+            for (int i = 0; i < cursussen.Length; i++)
+            {
+                if (cursussen[i] is not null)
+                {
+                    Console.WriteLine($"{cursussen[i]}:".PadRight(26) + CursusResultaten[i]);
+                }
+            }
+
+            Console.WriteLine($"Gemiddelde:".PadRight(26) + $"{Math.Round(Gemiddelde(cursussen), 1)}\n");
+        }
+
+        public static void DemonstreerStudenten()
+        {
+            Student student1 = new Student();
+            student1.GeboorteDatum = new DateTime(2001, 1, 3);
+            student1.Naam = "Said Aziz";
+            student1.RegistreerVoorCursus("Communicatie");
+            student1.CursusResultaten[0] = 12;
+            student1.RegistreerVoorCursus("Programmeren");
+            student1.CursusResultaten[1] = 15;
+            student1.RegistreerVoorCursus("Webtechnologie");
+            student1.CursusResultaten[2] = 13;
+            student1.ToonOverzicht();
+
+            Student student2 = new Student();
+            student2.GeboorteDatum = new DateTime(1996, 4, 23);
+            student2.Naam = "Mieke Vermeulen";
+            student2.RegistreerVoorCursus("Webontwikkeling");
+            student2.CursusResultaten[0] = 14;
+            student2.RegistreerVoorCursus("IT Project");
+            student2.CursusResultaten[1] = 11;
+            student2.RegistreerVoorCursus("IT Essentials");
+            student2.CursusResultaten[2] = 8;
+            student2.RegistreerVoorCursus("Programmeren Intro");
+            student2.CursusResultaten[3] = 18;
+            student2.ToonOverzicht();
         }
     }
 }
